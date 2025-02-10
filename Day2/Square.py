42027ecbfda592460e0d9625d4999d52482c40a0
RedoyRKS <redoyshaha06@gmail.com>
2025-02-10 17:16:03 +0600

    day_4

---
+++ Day2/Square.py
@@ -0,0 +1,9 @@
+# from function import square
+
+# for i in range(10):
+#     print(f"The square of {i} is {square(i*2)}")
+
+#another way to import
+import function
+for i in range(10):
+    print(f"The square of {i} is {function.square(i*2)}")
